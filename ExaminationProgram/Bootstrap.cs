fd18dec30afabd2fd67ffa800ff39590021570e7
TzeenchH <zenv99@mail.ru>
2019-11-11 20:33:15 +0300

    WIP = added main methods in bootstrap

--- ExaminationProgram/Bootstrap.cs
+++ ExaminationProgram/Bootstrap.cs
@@ -14,234 +14,108 @@ using System.Linq;
 using System.Text;
 using Autofac;
 using System.Threading.Tasks;
+using ExaminationProgram.Interfaces;
+using ExaminationProgram.Modules.CalibrationModule;
+using ExaminationProgram.Modules.LoggingModule;
+using MahApps.Metro.Controls;
 
 namespace ExaminationProgram
 {
     public class Bootstrap : IBootstrap
-    {       
-        public Bootstrap()
+    {
+        public IShell Initialize()
         {
-            var shell = new Shell("АИК");
-            //var InstrumentalView = new InstrumentalView();
-            //var SettingsView = new SettingsView();
-            //var MeasurementsView = new MeasurementsView();
-            //var DataBaseView = new DataBaseView();
-            //var LogsView = new LogsView();
-            //var CalibrationView = new CalibrationView();
-            //var WizardView = new WizardView();
-            var ContextMediator = new ContextMediator();
-            var SubStep1 = new ExecutableWizardStepBuilder()
-                .SetName("SubStep1.1")
-                .SetDescription("description for substep 1.1")
-                .SetReadOnly(false)
-                .SetSelectable(true)
-                .AddAction((s, c, o) =>
-                {
-                    try
-                    {
-
-                    }
-                    catch (System.Exception)
-                    {
-
-                        throw;
-                    }
-                    finally
-                    {
-
-                    }
-                })
-                .Build();
-
-            var SubStep2 = new ExecutableWizardStepBuilder()
-                .SetName("SubStep1.2")
-                .SetDescription("description for substep 1.2")
-                .SetReadOnly(false)
-                .SetSelectable(true)
-                .AddAction((s, c, o) =>
-                {
-                    try
-                    {
-
-                    }
-                    catch (System.Exception)
-                    {
-
-                        throw;
-                    }
-                    finally
-                    {
-
-                    }
-                })
-                .Build();
-
-            var SubSubStep1 = new ExecutableWizardStepBuilder()
-                .SetName("SubSubStep1.3.1")
-                .SetDescription("description for subsubstep 1.3.1")
-                .SetReadOnly(false)
-                .SetSelectable(true)
-                .AddAction((s, c, o) =>
-                {
-                    try
-                    {
-
-                    }
-                    catch (System.Exception)
-                    {
-
-                        throw;
-                    }
-                    finally
-                    {
-
-                    }
-                })
-                .Build();
-
-            var SubSubStep2 = new ExecutableWizardStepBuilder()
-                .SetName("SubSubStep1.3.2")
-                .SetDescription("description for subsubstep 1.3.2")
-                .SetReadOnly(false)
-                .SetSelectable(true)
-                .AddAction((s, c, o) =>
-                {
-                    try
-                    {
-
-                    }
-                    catch (System.Exception)
-                    {
-
-                        throw;
-                    }
-                    finally
-                    {
-
-                    }
-                })
-                .Build();
-
-            var GrouppedStep1_3 = new WizardStepGroupBuilder()
-                .SetName("SubGruop1.3")
-                .SetDescription("description for subgroup 1.3")
-                .AddChildren(SubSubStep1)
-                .AddChildren(SubSubStep2)
-                .Build();
-
-            var GrouppedStep1 = new WizardStepGroupBuilder()
-                .SetName("Group1")
-                .SetDescription("description for subgroup 1")
-                .AddChildren(SubStep1)
-                .AddChildren(SubStep2)
-                .AddChildren(GrouppedStep1_3)
-                .Build();
-
-            var Step2 = new ExecutableWizardStepBuilder()
-                .SetName("Step2")
-                .SetIconName("Settings")
-                .SetDescription("description for step 2")
-                .SetReadOnly(false)
-                .SetSelectable(true)
-                .AddBoundedStep(SubSubStep1)
-                .AddBoundedStep(GrouppedStep1)
-                .AddAction((s, c, o) =>
-                {
-                    try
-                    {
-
-                    }
-                    catch (System.Exception)
-                    {
-
-                        throw;
-                    }
-                    finally
-                    {
-
-                    }
-                })
-                .Build();
-
-            var SubStep3_1 = new ExecutableWizardStepBuilder()
-                .SetName("SubStep3.1")
-                .SetDescription("description for substep 3.1")
-                .SetReadOnly(false)
-                .SetSelectable(true)
-                .AddAction((s, c, o) =>
-                {
-                    try
-                    {
-
-                    }
-                    catch (System.Exception)
-                    {
-
-                        throw;
-                    }
-                    finally
-                    {
-
-                    }
-                })
-                .Build();
-
-            var GrouppedStep3 = new WizardStepGroupBuilder()
-                .SetName("Group3")
-                .SetDescription("description for group 3")
-                .AddChildren(SubStep3_1)
-                .Build();
-            var Step4 = new ExecutableWizardStepBuilder()
-                .SetName("Step4")
-                .SetIconName("Settings")
-                .SetDescription("description for step 2")
-                .SetReadOnly(false)
-                .SetSelectable(true)
-                .AddAction((s, c, o) =>
-                {
-                    try
-                    {
-
-                    }
-                    catch (System.Exception)
-                    {
-
-                        throw;
-                    }
-                    finally
-                    {
-
-                    }
-                })
-                .Last(true)
-                .Build();
-
-            var TestData = new TestData();
-            var SettingsGroupsContainer = new SettingsGroupsContainer(
-                new SettingsGroup("Group 1",
-                    new DoubleSetting() { Name = "Setting1.1", Dimention = "Hz", Visible = true },
-                    new IntSetting() { Name = "Setting1.2", Dimention = "Ohm", Visible = true }),
-                new SettingsGroup("Group 2",
-                    new DoubleSetting() { Name = "Setting2.1", Dimention = "dB", Visible = true },
-                    new IntSetting() { Name = "Setting2.2", Dimention = "Sec", Visible = true }),
-                new SettingsGroup("Group 3",
-                    new DoubleSetting() { Name = "Setting3.1", Dimention = "mV", Visible = true },
-                    new IntSetting() { Name = "Setting3.2", Dimention = "Rad", Visible = true }));
-
-            var InstrumentalViewModel = new InstrumentalViewModel("Модуль приборов", "appbar_power", ContextMediator, new SignalAnalyzer(), new SignalGenerator());
-            var SettingsViewModel = new SettingsViewModel("Модуль настроек", "appbar_settings", ContextMediator) { Groups = SettingsGroupsContainer.SettingsGroups };
-            var MeasurementsViewModel = new MeasurementsViewModel("Автоматический режим", "appbar_axis_x", ContextMediator, TestData) { SettingsList = SettingsGroupsContainer.SettingsGroups };
-            var DataBaseViewModel = new DataBaseViewModel("База данных", "appbar_database", ContextMediator);
-            var LogsViewModel = new LogsViewModel("Лог", "appbar_disk", ContextMediator);
-
-            var SetupWizard = new SetupWizardBuilder()
-                .AddStep(GrouppedStep1)
-                .AddStep(Step2)
-                .AddStep(GrouppedStep3)
-                .AddStep(Step4)
-                .ConfigureWizard()
-                .Build();
-
+            var container = ConfigureContainer();
+            var shell = container.Resolve<IShell>();
+            shell.Container = container;
+            var MainWindow = container.Resolve<MainWindow>() as MetroWindow;
+            return shell;
         }
+            
+        
+        private static IContainer ConfigureContainer()
+        {
+            var builder = new ContainerBuilder();
+            builder.RegisterType<MainWindow>()
+                .AsSelf()
+                .SingleInstance();
+            ConfigureCalibrationModule(builder);
+            ConfigureDatabaseModule(builder);
+            ConfigureInstrumentalModule(builder);
+            ConfigureLoggingWindow(builder);
+            ConfigureLogsModule(builder);
+            ConfigureMeasurementsModule(builder);
+            ConfigureSettingsModule(builder);
+            ConfigureWizard(builder);
+            return builder.Build();
+        }
+
+        private static void ConfigureLoggingWindow(ContainerBuilder builder)
+        {
+            builder.RegisterType<LogginWindowView>()
+                .AsSelf()
+                .SingleInstance();
+            builder.RegisterType<LoggingWindowViewModel>()
+                .AsSelf()
+                .SingleInstance();
+        }
+        private static void ConfigureCalibrationModule(ContainerBuilder builder)
+        {
+            builder.RegisterType<CalibrationViewModel>()
+                .As<IViewModel>();
+            builder.RegisterType<CalibrationView>()
+                .As<IView>();
+        }
+
+        private static void ConfigureDatabaseModule(ContainerBuilder builder)
+        {
+            builder.RegisterType<DataBaseViewModel>()
+                .As<IViewModel>();
+
+            builder.RegisterType<DataBaseView>()
+               .As<IView>();               
+        }
+           
+
+        private static void ConfigureInstrumentalModule(ContainerBuilder builder)
+        {
+            builder.RegisterType<InstrumentalViewModel>()
+                .As<IViewModel>();
+            builder.RegisterType<InstrumentalView>()
+                .As<IView>();
+        }
+
+        private static void ConfigureLogsModule(ContainerBuilder builder)
+        {
+            builder.RegisterType<LogsViewModel>()
+                .As<IViewModel>();
+            builder.RegisterType<LogsView>()
+                .As<IView>();
+        }
+        private static void ConfigureMeasurementsModule(ContainerBuilder builder)
+        {
+            builder.RegisterType<MeasurementsViewModel>()
+                .As<IViewModel>();
+            builder.RegisterType<MeasurementsView>()
+                .As<IView>();
+        }
+        private static void ConfigureSettingsModule(ContainerBuilder builder)
+        {
+            builder.RegisterType<SettingsViewModel>()
+                .As<IViewModel>();
+            builder.RegisterType<SettingsView>()
+                .As<IView>();
+        }
+
+        private static void ConfigureWizard(ContainerBuilder builder)
+        {
+            builder.RegisterType<WizardView>()
+                .AsSelf()
+                .SingleInstance();
+            builder.RegisterType<SetupWizard>()
+                .AsSelf()
+                .SingleInstance();
+        }
+        
     }
 }
---
+++ ExaminationProgram/Interfaces/IShell.cs
@@ -0,0 +1,14 @@
+﻿using Autofac;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExaminationProgram.Interfaces
+{
+    public interface IShell
+    {
+        IContainer Container { get; set; }
+    }
+}
